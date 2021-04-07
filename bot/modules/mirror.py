bc7a3786e94f4a29087fc3db29f26202b493166a
RAHUL112KAPOOR <68430011+RAHUL112KAPOOR@users.noreply.github.com>
2021-04-07 19:02:03 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler, run_async
 from telegram import InlineKeyboardMarkup
 
 from bot import Interval, INDEX_URL, BUTTON_THREE_NAME, BUTTON_THREE_URL, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, BLOCK_MEGA_LINKS
-from bot import dispatcher, DOWNLOAD_DIR, DOWNLOAD_STATUS_UPDATE_INTERVAL, download_dict, download_dict_lock
+from bot import dispatcher, DOWNLOAD_DIR, DOWNLOAD_STATUS_UPDATE_INTERVAL, download_dict, download_dict_lock, SHORTENER, SHORTENER_API
 from bot.helper.ext_utils import fs_utils, bot_utils
 from bot.helper.ext_utils.bot_utils import setInterval
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException, NotSupportedExtractionArchive
@@ -114,7 +114,7 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onDownloadError(self, error):
         error = error.replace('<', ' ')
-        error = error.replace('>', ' ')        
+        error = error.replace('>', ' ')
         LOGGER.info(self.update.effective_chat.id)
         with download_dict_lock:
             try:
@@ -148,13 +148,27 @@ class MirrorListener(listeners.MirrorListeners):
         with download_dict_lock:
             msg = f'<b>Filename : </b><code>{download_dict[self.uid].name()}</code>\n<b>Sɪᴢᴇ : </b><code>{download_dict[self.uid].size()}</code>'
             buttons = button_build.ButtonMaker()
-            buttons.buildbutton("☁️ Drive Link ☁️", link)
+            if self.message.from_user.username:
+                uname = f"@{self.message.from_user.username}" # Username Looks Great!
+            else:
+                uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>' # HTML Format!
+            if uname is not None:
+                msg += f'\n\nUploaded by {uname}' # Done!
+            if SHORTENER is not None and SHORTENER_API is not None:
+                surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, link)).text
+                buttons.buildbutton("☁️ Drive Link ☁️", surl)
+            else:
+                buttons.buildbutton("☁️ Drive Link ☁️", link)
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 share_url = requests.utils.requote_uri(f'{INDEX_URL}/{download_dict[self.uid].name()}')
                 if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                     share_url += '/'
-                buttons.buildbutton("⚡ Index Link ⚡", share_url)
+                if SHORTENER is not None and SHORTENER_API is not None:
+                    siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, share_url)).text
+                    buttons.buildbutton("⚡ Index Link ⚡", siurl)
+                else:
+                    buttons.buildbutton("⚡ Index Link ⚡", share_url)
             if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                 buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
@@ -199,6 +213,7 @@ def _mirror(bot, update, isTar=False, extract=False):
     message_args = update.message.text.split(' ')
     name_args = update.message.text.split('|')
     try:
+        link = message_args[1]
         if link.startswith("|") or link.startswith("pswd: "):
             link = ''
     except IndexError:
